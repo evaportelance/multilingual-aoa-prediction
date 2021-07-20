d3949aa6b690cbd67457707732f7b5cae71af039
ibevers <isaacbevers@gmail.com>
2021-07-19 17:18:02 -0700

    Work on dataloader.

--- Code/Python/create_toy_dataset.py
+++ Code/Python/create_toy_dataset.py
@@ -1,23 +1,41 @@
 import random
 
 MODEL_SETS_DIR = "../../Data/model-sets"
-
+TRAIN_NUM_LINES = 100
+VALIDATION_NUM_LINES = 30
+TEST_NUM_LINES = 10
 
 #Open text files
 #Get random lines
 #Save random lines to a new file
 #Path to directory where the final test, train, and validation sets will be stored
 
-train_set = []
-validation_set = []
-test_set = []
+toy_train_set = []
+toy_validation_set = []
+toy_test_set = []
 with open(MODEL_SETS_DIR + '/train.txt','r') as f :
-    for line in f:
-       train_set.append(line)
-with open(MODEL_SETS_DIR + '/validation.txt','r') as f :
-    for line in f:
-       validation_set.append(line)
-with open(MODEL_SETS_DIR + '/test.txt','r') as f :
-    for line in f:
-       test_set.append(line)
+    for i in range(TRAIN_NUM_LINES):
+        line_num = random.randint(0, 1000)
+        toy_train_set.append(f.readline(line_num))
 
+with open(MODEL_SETS_DIR + "/toy_train.txt", 'w') as f:
+    for line in toy_train_set:
+        f.write(line)
+
+with open(MODEL_SETS_DIR + '/validation.txt','r') as f :
+    for i in range(VALIDATION_NUM_LINES):
+        line_num = random.randint(0, 1000)
+        toy_validation_set.append(f.readline(line_num))
+
+with open(MODEL_SETS_DIR + "/toy_validation.txt", 'w') as f:
+    for line in toy_validation_set:
+        f.write(line)
+
+with open(MODEL_SETS_DIR + '/test.txt','r') as f :
+    for i in range(TEST_NUM_LINES):
+        line_num = random.randint(0, 1000)
+        toy_test_set.append(f.readline(line_num))
+
+with open(MODEL_SETS_DIR + "/toy_test.txt", 'w') as f:
+    for line in toy_test_set:
+        f.write(line)
--- Code/Python/data_loader.py
+++ Code/Python/data_loader.py
@@ -3,13 +3,14 @@ What am I doing today
 steps:
     *set up pytorch
     *Set up Keras and PyTorch
-    Create DataLoader
+    *Initialize data classes
+    *Pad data
+    *Create DataLoaders
 What does the dataloader do?
 Loads data to model for training
 
 Input: data as a txt
 Output: batches of data
-
 
 Pad data, length of longest
 load 5000 most common words
@@ -21,17 +22,71 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 
-TRAINING_DATA_PATH = "../../Data/model-sets/train.txt"
-VALIDATION_DATA_PATH = "../../Data/model-sets/validation.txt"
-TEST_DATA_PATH = "../../Data/model-sets/test.txt"
-
-
-'''
+TRAINING_DATA_PATH = "../../Data/model-sets/toy_train.txt"
+VALIDATION_DATA_PATH = "../../Data/model-sets/toy_validation.txt"
+TEST_DATA_PATH = "../../Data/model-sets/toy_test.txt"
 
 '''
+'''
+# class FaceLandmarksDataset(Dataset):
+#     """Face Landmarks dataset."""
+#
+#     def __init__(self, csv_file, root_dir, transform=None):
+#         """
+#         Args:
+#             csv_file (string): Path to the csv file with annotations.
+#             root_dir (string): Directory with all the images.
+#             transform (callable, optional): Optional transform to be applied
+#                 on a sample.
+#         """
+#         self.landmarks_frame = pd.read_csv(csv_file)
+#         self.root_dir = root_dir
+#         self.transform = transform
+#
+#     def __len__(self):
+#         return len(self.landmarks_frame)
+#
+#     def __getitem__(self, idx):
+#         if torch.is_tensor(idx):
+#             idx = idx.tolist()
+#
+#         img_name = os.path.join(self.root_dir,
+#                                 self.landmarks_frame.iloc[idx, 0])
+#         image = io.imread(img_name)
+#         landmarks = self.landmarks_frame.iloc[idx, 1:]
+#         landmarks = np.array([landmarks])
+#         landmarks = landmarks.astype('float').reshape(-1, 2)
+#         sample = {'image': image, 'landmarks': landmarks}
+#
+#         if self.transform:
+#             sample = self.transform(sample)
+#
+#         return sample
+
+#File open function
+
+train_data = []
+with open(TRAINING_DATA_PATH, "r") as f:
+    for line in f:
+        train_data.append(line.split())
+
+validation_data = []
+with open(TRAINING_DATA_PATH, "r") as f:
+    for line in f:
+        validation_data.append(line.split())
+
+test_data = []
+with open(TRAINING_DATA_PATH, "r") as f:
+    for line in f:
+        test_data.append(line.split())
+
+train_word_set = set(train_data)
+word_index_dict = {}
+train_set = word_index_dict.fromkeys(train_word_set[, 0])
+
 class TrainingSet(Dataset):
-    def __init__(self):
-        self.data = pd.read_csv(TRAINING_DATA_PATH, sep='\n')
+    def __init__(self, path = TRAINING_DATA_PATH):
+        self.data = test
 
     def __len__(self):
         return len(self.data)
@@ -39,20 +94,30 @@ class TrainingSet(Dataset):
     def __getitem__(self, index):
         return self.data[index]
 
-
 class ValidationSet(Dataset):
-    def __init__(self):
-        self.data = pd.read_csv(VALIDATION_DATA_PATH, sep='\n')
+    def __init__(self, path = VALIDATION_DATA_PATH):
+        with open(path, "r") as f:
+            keys = f.readlines()
+            maxlen = len(max(keys, key=len))
+            keys = [line.rjust(maxlen) for line in keys]
+            values = range(len(keys))
+            self.data = dict(zip(keys, values))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
+
         return self.data[index]
 
 class TestSet(Dataset):
-    def __init__(self):
-        self.data = pd.read_csv(TEST_DATA_PATH, sep='\n')
+    def __init__(self, path = TEST_DATA_PATH):
+        with open(path, "r") as f:
+            keys = f.readlines()
+            maxlen = len(max(keys, key=len))
+            keys = [line.rjust(maxlen) for line in keys]
+            values = range(len(keys))
+            self.data = dict(zip(keys, values))
 
     def __len__(self):
         return len(self.data)
@@ -60,22 +125,36 @@ class TestSet(Dataset):
     def __getitem__(self, index):
         return self.data[index]
 
-train_max_len_line = df.col1.map(lambda TrainingSet.data: len(TrainingSet.data)).max()
-TrainingSet.data = TrainingSet.data.str.pad(train_max_len_line, side ='left')
-valid_max_len_line = df.col1.map(lambda ValidationSet.data: len(ValidationSet.data)).max()
-TrainingSet.data = ValidationSet.data.str.pad(valid_max_len_line, side ='left')
-test_max_len_line = df.col1.map(lambda TestSet.data: len(TestSet.data)).max()
-TrainingSet.data = TestSet.data.str.pad(test_max_len_line, side ='left')
+#Initialize a dataloader for each data set
+training_dataloader = TrainingSet()
+validation_dataloader = ValidationSet()
+test_dataloader = TestSet()
 
-train_dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True)
+'''
 
-valid_dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True)
 
-test_dataloader = DataLoader(transformed_dataset, batch_size=4,
-                        shuffle=True)
+'''
 
+
+
+
+# train_max_len_line = .col1.map(lambda TrainingSet.data: len(TrainingSet.data)).max()
+# TrainingSet.data = TrainingSet.data.str.pad(train_max_len_line, side ='left')
+#
+# valid_max_len_line = df.col1.map(lambda ValidationSet.data: len(ValidationSet.data)).max()
+# TrainingSet.data = ValidationSet.data.str.pad(valid_max_len_line, side ='left')
+# test_max_len_line = df.col1.map(lambda TestSet.data: len(TestSet.data)).max()
+# TrainingSet.data = TestSet.data.str.pad(test_max_len_line, side ='left')
+#
+# train_dataloader = DataLoader(transformed_dataset, batch_size=4,
+#                         shuffle=True)
+#
+# valid_dataloader = DataLoader(transformed_dataset, batch_size=4,
+#                         shuffle=True)
+#
+# test_dataloader = DataLoader(transformed_dataset, batch_size=4,
+#                         shuffle=True)
+#
 
 
 
