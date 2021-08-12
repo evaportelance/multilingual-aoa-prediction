0f2ef3b34228810ade5ad4c43fde41102cf7ced3
Eva Portelance <portelance.eva@gmail.com>
2021-08-12 10:45:02 -0700

    adding scripts for finetuning multilingual bert

---
+++ Code/Python/bert_custom_dataset.py
@@ -0,0 +1,31 @@
+import torch
+from torch.utils.data import Dataset
+from transformers import BertTokenizer
+
+## Custom Dataset class for CHILDES utterances
+class CHILDESDataset(Dataset):
+    def __init__(self, file_path):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased')
+
+        self.sentences = []
+        with open(file_path, "r") as f:
+            for line in f:
+                text = line.strip()
+                self.sentences.append(text)
+        encoded_data = self.tokenizer(self.sentences, return_tensors='pt', padding=True, truncation=True)
+
+        self.input_ids = encoded_data['input_ids']
+        self.token_type_ids = encoded_data['token_type_ids']
+        self.attention_mask = encoded_data['attention_mask']
+
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, index):
+        batch_dict = {'input_ids': self.input_ids[index],
+                      'token_type_ids': self.token_type_ids[index],
+                      'attention_mask': self.attention_mask[index],
+                      'labels': self.input_ids[index]}
+
+        return batch_dict
---
+++ Code/Python/multiling_bert_finetune.py
@@ -0,0 +1,102 @@
+import argparse
+import os
+import torch
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from transformers import BertLMHeadModel, AdamW
+from stats import AverageMeterSet, StatTracker
+from bert_custom_dataset import CHILDESDataset
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_path', type=string, default='../../Data/model-sets/train.txt',
+                        help='file path for training data')
+    parser.add_argument('--val_path', type=string, default='../../Data/model-sets/validation.txt',
+                        help='file path for validation data')
+    parser.add_argument('--result_dir', type=string, default='../../Results/Finetune_experiments/',
+                        help='directory where model and log files will be saved')
+    parser.add_argument('--lr', type=double, default='5e-5',
+                        help='learning rate')
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--n_epochs', type=int, default=30)
+    parser.add_argument('--patience', type=int, default=5)
+
+    args = parser.parse_args()
+    return args
+
+
+## Evaluation function for getting accuracy using argmax of token predictions
+def test_finetuned_model(model, dataloader, device, stat_tracker, epoch=1, prefix='test'):
+    model.eval()
+    test_stats = AverageMeterSet()
+    batch_size = dataloader.batch_size
+    for batch in dataloader:
+        for key in batch:
+            batch[key] = batch[key].to(device)
+        labels = batch['labels']
+        outputs = model(**batch)
+        distributions = F.log_softmax(outputs.logits, -1)
+        predictions = torch.argmax(distributions, dim = -1)
+        n_matches = torch.eq(predictions, labels).int()
+        max_sequence_len = list(n_matches.size())[1]
+        avg_sequence_accs = torch.sum(n_matches, 1)/max_sequence_len
+        batch_accuracy = (torch.sum(avg_sequence_accs, 0)/avg_sequence_accs.size()[0]).item()
+        test_stats.update('accuracy', batch_accuracy, n=1)
+    stat_tracker.record_stats(test_stats.averages(epoch, prefix=prefix))
+
+    return test_stats.avgs['accuracy']
+
+
+## Train function for finetuning multilingual BERT
+def finetune_model(model, train_dataloader, val_dataloader, device, stat_tracker, n_epochs=30, lr=5e-5, patience=5):
+    optimizer = AdamW(model.parameters(), lr)
+    stop_early_count = 0
+    previous_val_acc = 0.0
+    for epoch in range(n_epochs):
+        model.train()
+        epoch_stats = AverageMeterSet()
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            for key in batch:
+                batch[key] = batch[key].to(device)
+            outputs = model(**batch)
+            loss = outputs[0]
+            loss.backward()
+            optimizer.step()
+            epoch_stats.update('loss', loss, n=1)
+        val_accuracy = test_finetuned_model(model, val_dataloader, device, stat_tracker, epoch, prefix="val")
+        stat_tracker.record_stats(epoch_stats.averages(epoch, prefix="train"))
+        print(str(epoch))
+        print("val acc: "+ str(val_accuracy))
+        print("loss :"+ str(epoch_stats.avgs['loss']))
+        if previous_val_acc >= val_accuracy:
+            stop_early_count +=1
+        else:
+            stop_early_count = 0
+        if stop_early_count >= patience:
+            break
+        previous_val_acc = val_accuracy
+
+    return model
+
+
+def main():
+    args = get_args()
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = BertLMHeadModel.from_pretrained("bert-base-multilingual-uncased", return_dict=True, is_decoder = True)
+    model = model.to(device)
+
+    train_dataset = CHILDESDataset(args.train_path)
+    train_dl = DataLoader(train_dataset, batch_size=args.batch_size,shuffle=True)
+    val_dataset = CHILDESDataset(args.val_path)
+    val_dl = DataLoader(val_dataset, batch_size=args.batch_size,shuffle=True)
+
+    stat_tracker = StatTracker(log_dir=os.path.join(args.result_dir,"tensorboard-log"))
+
+    model = finetune_model(model, train_dl, val_dl, device, stat_tracker, args.n_epochs, args.lr, args.patience)
+
+    torch.save(model, os.path.join(args.result_dir,"model.pt"))
+
+
+if __name__=="__main__":
+    main()
